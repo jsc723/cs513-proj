ac726ac12048a9ca211ab66e7c649458b810f42f
Yunado <36465036+Yunado@users.noreply.github.com>
2023-07-29 14:17:27 -0400

    add tweet handle

--- query.py
+++ query.py
@@ -12,11 +12,12 @@ def main():
     df = ps.sqldf('''SELECT * FROM df WHERE country ="US"''')
     print(df)
 
-    review_count_df = ps.sqldf('''SELECT taster_name, count(*) as num_of_reviews FROM df GROUP BY taster_name ORDER BY num_of_reviews DESC''')
+    review_count_df = ps.sqldf('''SELECT taster_name, taster_twitter_handle, count(*) as num_of_reviews FROM df GROUP BY taster_name ORDER BY num_of_reviews DESC''')
     print(review_count_df)
     most_significant_reviewer = review_count_df['taster_name'][0]
+    twitter_handle = review_count_df['taster_twitter_handle'][0]
     num_of_reviews = review_count_df['num_of_reviews'][0]
-    print(f'most_significant_reviewer: "{most_significant_reviewer}", num_of_reviews: {num_of_reviews}')
+    print(f'most_significant_reviewer: "{most_significant_reviewer}": {twitter_handle}, num_of_reviews: {num_of_reviews}')
 
     result_df = ps.sqldf(f'''SELECT variety, count(*) as num_of_reviews, avg(normalized_points) as avg_points, min(price) as min_price, max(price) as max_price
              FROM df WHERE taster_name = "{most_significant_reviewer}" GROUP BY variety''')
